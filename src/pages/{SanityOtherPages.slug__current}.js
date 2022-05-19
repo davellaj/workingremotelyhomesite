8a1534b605f4e860c5a0ee699511b11946b68fa0
Jamie Davella <jamie.davella@gmail.com>
2022-05-19 15:06:39 -0400

    center privacy and tos page with max-width

--- src/pages/{SanityOtherPages.slug__current}.js
+++ src/pages/{SanityOtherPages.slug__current}.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import PortableText from '@sanity/block-content-to-react';
+import styled from '@emotion/styled';
 
+const ContentWrapper = styled.article`
+    max-width: 1200px;
+    margin: 0 auto;
+
+    & a {
+        text-decoration: none;
+    }
+`;
 export const query = graphql`
     query SanityOtherPages($id: String!) {
         sanityOtherPages(id: { eq: $id }) {
@@ -17,8 +26,10 @@ export default function SanityOtherPages({ data }) {
     const page = data.sanityOtherPages;
     return (
         <Layout title={page.title} description={page.description}>
-            <h1>{page.title}</h1>
-            <PortableText blocks={page?._rawBody} />
+            <ContentWrapper>
+                <h1>{page.title}</h1>
+                <PortableText blocks={page?._rawBody} />
+            </ContentWrapper>
         </Layout>
     );
 }
