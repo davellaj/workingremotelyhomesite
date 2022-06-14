45028935501fc778d250de702ab7344a1fc75e1c
Jamie Davella <jamie.davella@gmail.com>
2022-06-13 22:19:24 -0400

    add gatsby-plugin-google-gtag with GA tracking id and optimize_id to try and make google analytics work with gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,36 @@ module.exports = {
                 trackingId: 'G-XRLZ43DEWY',
             },
         },
+        // gatsby-plugin-google-analytics was not working with google analytics.
+        // Added this package https://www.mariokandut.com/how-to-add-google-analytics-in-gatsby/#:~:text=Add%20Google%20Analytics%20via%20official%20plugin&text=Add%20the%20plugin%20to%20your,fired%20for%20all%20of%20them.
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                // You can add multiple tracking ids and a pageview event will be fired for all of them.
+                trackingIds: [
+                    'G-XRLZ43DEWY', // Google Analytics / GA
+                    // optional
+                    // 'OPTIONAL----AW-CONVERSION_ID', // Google Ads / Adwords / AW
+                    // 'OPTIONAL----DC-FLOODIGHT_ID', // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+                ],
+                // This object gets passed directly to the gtag config command
+                // This config will be shared across all trackingIds
+                gtagConfig: {
+                    optimize_id: 'OPT-TZD7FNK',
+                    anonymize_ip: true,
+                    cookie_expires: 0,
+                },
+                // This object is used for configuration specific to this plugin
+                pluginConfig: {
+                    // Puts tracking script in the head instead of the body
+                    head: false,
+                    // Setting this parameter is also optional
+                    respectDNT: true,
+                    // Avoids sending pageview hits from custom paths
+                    //exclude: ['/preview/**', '/do-not-track/me/too/'],
+                },
+            },
+        },
         'gatsby-plugin-image',
         'gatsby-plugin-react-helmet',
         'gatsby-plugin-sitemap',
